b620a053a311aaa902ab79fe40d440869eebd4f7
longqing <zze@live.com>
2025-07-03 12:28:53 +0800

    Add IsValid20 class and unit tests for validation

---
+++ src/LeetCode/IsValid20.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode;
+public class IsValid20
+{
+    public bool IsValid(string s)
+    {
+        var symbols = new Dictionary<char, char>
+        {
+            { '(', ')' },
+            { '{', '}' },
+            { '[', ']' }
+        };
+        var stack = new Stack<char>();
+
+        foreach (var c in s)
+        {
+            if (symbols.ContainsKey(c))
+            {
+                stack.Push(c);
+            }
+
+            if (symbols.ContainsValue(c))
+            {
+                if (!stack.Any())
+                {
+                    return false;
+                }
+
+                if (c != symbols[stack.Peek()])
+                {
+                    return false;
+                }
+                stack.Pop();
+            }
+        }
+
+        return !stack.Any();
+    }
+}
---
+++ tests/LeetCode.Tests/IsValid20Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests;
+public class IsValid20Test
+{
+    [Fact]
+    public void TestIsValid()
+    {
+        var result = new IsValid20().IsValid("()[]{}");
+
+        Assert.True(result);
+    }
+}
