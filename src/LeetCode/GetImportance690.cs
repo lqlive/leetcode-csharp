f95b3b3f57878cc99cfda1e54aadfb6c93d56984
lqlive <zze@live.com>
2025-08-02 20:06:35 +0800

    Add FindLongestSubstring and MergeIntervals classes with unit tests

---
+++ src/LeetCode/GetImportance690.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode;
+
+public class GetImportance690
+{
+    Dictionary<int, Employee> employeeMap = new Dictionary<int, Employee>();
+    public int GetImportance(IList<Employee> employees, int id)
+    {
+        employeeMap = employees.ToDictionary(e => e.id);
+
+        return DFS(id);
+    }
+
+    public int DFS(int id)
+    {
+        var employee = employeeMap[id];
+        var total = employee.importance;
+
+        foreach (var item in employee.subordinates)
+        {
+            total += DFS(item);
+        }
+        return total;
+    }
+}
+
+public class Employee
+{
+    public int id;
+    public int importance;
+    public IList<int> subordinates;
+}
---
+++ tests/LeetCode.Tests/GetImportance690Test.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests
+{
+    public class GetImportance690Test
+    {
+        [Fact]
+        public void TestGetImportance()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee
+                {
+                    id = 1,
+                    importance = 5,
+                    subordinates = []
+                }
+            };
+            var result = new GetImportance690().GetImportance(employees, 1);
+
+            Assert.Equal(5, result);
+        }
+    }
+}
