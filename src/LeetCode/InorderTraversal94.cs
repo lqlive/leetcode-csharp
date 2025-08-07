1f8d120b395b2030c6bb6b5a20cdebb8f3f8100f
lqlive <zze@live.com>
2025-08-07 23:30:51 +0800

    Add InorderTraversal94

---
+++ src/LeetCode/InorderTraversal94.cs
@@ -0,0 +1,29 @@
+﻿using LeetCode.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode;
+
+public class InorderTraversal94
+{
+    public IList<int> InorderTraversal(TreeNode root)
+    {
+        var result= new List<int>();
+        Inorder(root, result);
+        return result;
+    }
+
+    public void Inorder(TreeNode root,List<int> result)
+    {
+        if (root is null)
+        {
+            return;
+        }
+        Inorder(root.left, result);
+        result.Add(root.val);
+        Inorder(root.right, result);
+    }
+}
