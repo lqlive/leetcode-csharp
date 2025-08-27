f06a3c09cf9b7e16746d6d34f3d1b9cebb26f6db
longqing <zze@live.com>
2025-08-27 18:18:35 +0800

    Add CombinationSum39 class and unit tests
    Introduces the `CombinationSum39` class with a method to find all unique combinations of candidates that sum to a target value using a depth-first search (DFS) approach. Also adds a test class `CombinationSum39Test` with a unit test to validate the functionality of the `CombinationSum` method.

---
+++ src/LeetCode/CombinationSum39.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode;
+public class CombinationSum39
+{
+    public IList<IList<int>> CombinationSum(int[] candidates, int target)
+    {
+        var ans = new List<IList<int>>();
+        var combine = new List<int>();
+        DFS(candidates, target, ans, combine, 0);
+        return ans;
+    }
+
+    private void DFS(int[] candidates,int target, List<IList<int>> ans,List<int> combine,int index)
+    {
+        if (index == candidates.Length)
+        {
+            return;
+        }
+
+        if (target == 0)
+        {
+            ans.Add(new List<int>(combine));
+            return;
+        }
+
+        DFS(candidates, target, ans, combine, index + 1);
+
+        if (target - candidates[index] >= 0)
+        {
+            combine.Add(candidates[index]);
+
+            DFS(candidates, target - candidates[index], ans, combine, index);
+
+            combine.RemoveAt(combine.Count - 1);
+        }
+    }
+}
---
+++ tests/LeetCode.Tests/CombinationSum39Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Tests;
+public class CombinationSum39Test
+{
+    [Fact]
+    public void TestCombinationSum()
+    {
+        var result = new CombinationSum39().CombinationSum([2, 3, 6], 7);
+        Assert.Single(result);
+    }
+}
